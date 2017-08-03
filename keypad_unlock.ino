82a3a853a4851a24c80d94cb5f7c45d4bb58040e
shubhamgupta2502 <shubhamgupta00524@gmail.com>
2017-08-03 18:43:29 +0530

    Update keypad_unlock.ino

--- keypad_unlock.ino
+++ keypad_unlock.ino
@@ -1,11 +1,15 @@
-#include <Password.h>
-#include <Keypad.h>
 #include <LiquidCrystal.h>
+#include <Keypad.h>
+#include <Password.h>
 LiquidCrystal lcd(12, 11, A3, A2, A1, A0);
-Password password = Password( "1111" );
 
-const byte ROWS = 4; 
+Password password = Password( "1234" );
+
+byte maxPasswordLength = 4; 
+byte currentPasswordLength = 0;
+const byte ROWS = 4;
 const byte COLS = 3; 
+
 char keys[ROWS][COLS] = {
   {'1','2','3'},
   {'4','5','6'},
@@ -14,66 +18,63 @@ char keys[ROWS][COLS] = {
 };
 byte rowPins[ROWS] = {5, 4, 3, 2}; 
 byte colPins[COLS] = {8, 7, 6}; 
-
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup(){
-
-  pinMode(9, OUTPUT);
-  
+   pinMode(9, OUTPUT);// yellow
+  pinMode(10, OUTPUT);// red
   Serial.begin(9600);
-   keypad.addEventListener(keypadEvent);
    lcd.begin(16, 2);
-   lcd.print("Please Enter Password:");
+   lcd.print("Enter Password:");
    lcd.setCursor(0, 1);
 }
-  
+
 void loop(){
-  
-  char key = keypad.getKey();
-  keypad.getKey();
-  
-  if (key){
-    
-    lcd.print(key);
-    
-    
-  }
-  
-  
+   char key = keypad.getKey();
+   if (key != NO_KEY){
+      delay(60); 
+      switch (key){
+      case '*': checkPassword(); 
+      break;
+      case '#': resetPassword();
+       lcd.clear();
+    lcd.print("Enter Password:");
+    password.reset();
+    lcd.setCursor(0,1);
+      break;
+      default: processNumberKey(key);
+      }
+   }
 }
 
-void keypadEvent(KeypadEvent eKey){
-  switch (keypad.getState()){
-    case PRESSED:
-  
-  Serial.println(eKey);
-  switch (eKey){
-    case '*':
-    pwdcheck();
-    break;
-    
-    case '#': 
-   password.reset();
-   lcd.clear();
-    lcd.print("Please Enter Password:");
-    lcd.setCursor(0, 1);
-   
-    
-    default:
-    password.append(eKey);
-     }
-  }
+void processNumberKey(char key) {
+   lcd.print(key);
+   currentPasswordLength++;
+   password.append(key);
+   if (currentPasswordLength == maxPasswordLength) {
+      checkPassword();
+   } 
 }
 
-void pwdcheck(){
-  if (password.evaluate()){
-    lcd.print("Success Wellcome to Robo Inventors");
-   
-  }else{
-    lcd.print("Sorry Password is Wrong");
-    digitalWrite(9,HIGH);
-   
-  }
+void checkPassword() {
+   if (password.evaluate()){
+      lcd.print(" OK.");
+       digitalWrite(9,HIGH);
+    delay(2000);
+    digitalWrite(9,LOW);
+      
+   } else {
+      lcd.print(" Sorry Wrong passwowrd!");
+      digitalWrite(10,HIGH);
+    delay(2000);
+    digitalWrite(10,LOW);
+      
+   } 
+   resetPassword();
+}
+
+void resetPassword() {
+   password.reset(); 
+   currentPasswordLength = 0; 
 }
 
